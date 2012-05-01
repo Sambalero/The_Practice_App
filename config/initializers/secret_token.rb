f228c696bc66cffc3a013297d0f73a4f7a48ce8d
Kevin Swallow <kevinswallow2@hotmail.com>
2012-05-01 10:57:19 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+H33::Application.config.secret_token = '3a5ecfbbd0bc3ca008df9bad3aefbec302fa44cccff4b07d15564f8fd1dbc0fa76dc3aa85610db1116b0ffb162bc2a11d6d4e0865358b7887764dc7b9a3b691e'
