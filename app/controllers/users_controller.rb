e28b7972bbd8ab64c701fe516225f14581c14981
Sambalero <kevinswallow2@hotmail.com>
2012-10-24 12:04:56 -0700

    9.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 before_filter :signed_in_user, only: [:edit, :update]
+before_filter :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id]) #on create, @user is passed to show, 
@@ -21,12 +22,11 @@ before_filter :signed_in_user, only: [:edit, :update]
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
+  def edit 
   end
 
   def update
-    @user = User.find(params[:id])
+
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -36,11 +36,19 @@ before_filter :signed_in_user, only: [:edit, :update]
     end
   end
 
-    private
+  private
 
-    def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in." 
       #shortcut for setting flash[:notice], (works for the :error key, but not for :success.)
-    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+end
   
 end
