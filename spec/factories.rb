3e41bb943e71cf3ca25a348d0fcc6e4457bb3025
Sambalero <kevinswallow2@hotmail.com>
2013-03-11 10:10:18 -0700

    Finish updating

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
     sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do #"we can now use FactoryGirl.create(:admin) to create an administrative user": don't seem right, but...
+      admin true
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,8 +25,11 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
-  it { should be_valid }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+  it { should be_valid }
+  it { should_not be_admin }
+
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -78,6 +81,15 @@ describe User do
 	  before { @user.password_confirmation = "mismatch" }
 	  it { should_not be_valid }
 	end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
 	describe "when password confirmation is nil" do
 	  before { @user.password_confirmation = nil }
